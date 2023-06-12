eacadc2ec686c5b7219c90dca074cc2ea9f09293
Timur Dolgikh <89405961+DolgikhTi@users.noreply.github.com>
2023-06-12 18:00:23 +0300

    Add files via upload

---
+++ WordGrouping/src/main/kotlin/Main.kt
@@ -0,0 +1,31 @@
+fun main(args: Array<String>) {
+    /**
+     * Sample Input ["eat", "tea", "tan", "ate", "nat", "bat"]
+    Sample Output [ ["ate", "eat", "tea"], ["nat", "tan"], ["bat"] ]
+    Т.е. сгруппировать слова по "общим буквам".
+     */
+
+    println(groupWords(arrayOf("eat", "tea", "tan", "ate", "nat", "bat")))
+}
+
+fun groupWords(words: Array<String>): List<List<String>> {
+    val result: MutableList<List<String>> = mutableListOf()
+
+    val map = mutableMapOf<String, MutableList<String>>()
+
+    for (word in words) {
+        // ['a', 'b', 'c'].joinToString(-) -> 'a-b-c'
+        val sortedWord = word.toCharArray().sorted().joinToString("")
+
+        if (map.containsKey(sortedWord))
+            map[sortedWord]?.add(word)
+        else
+            map[sortedWord] = mutableListOf(word)
+    }
+
+    for ((key, value) in map) {
+        result.add(value)
+    }
+
+    return result
+}
